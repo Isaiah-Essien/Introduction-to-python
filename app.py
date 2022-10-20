e800c21e4fa86a53008d60ad44ce58bfe26c4ab1
Isaiah-Essien <essienisaiah.732@gmail.com>
2022-10-20 09:47:00 +0200

    introduction to python variables

---
+++ app.py
@@ -0,0 +1,19 @@
+from re import X
+
+
+print("isaiah")
+if 5>2:
+ print("five is greater than two")
+ price=10
+ price=9
+print(price)
+a=4
+b=("7")
+c=8.4
+print(type(a))
+print(type(b))
+print(type(c))
+X=4
+x=("4")
+print(X)
+print(x)
